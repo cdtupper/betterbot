57541630b44a95959a2350a86160016ed39dcb81
Cullum Smith <cullum@c0ffee.net>
2014-01-30 00:00:29 -0500

    added comments to sample plugin

--- lib/Bot/BetterBot/Plugin/Echo.pm
+++ lib/Bot/BetterBot/Plugin/Echo.pm
@@ -8,12 +8,19 @@ use Moose;
 
 extends 'Bot::BetterBot::Plugin';
 
+
+# this function will be called whenever a user speaks in a channel we're in,
+# or sends us a private message
 override on_msg => sub {
    my ($self, $msg) = @_;
+
+   # check is message has the form "!command args" or "botname: command args"
    my ($cmd, $args) = $self->parse_cmd($msg);
 
    if ($cmd eq 'echo') {
       return "You must specify a string to echo." unless $args;
+      
+      # reply with echo of user's message
       return $args;
    }
 };
